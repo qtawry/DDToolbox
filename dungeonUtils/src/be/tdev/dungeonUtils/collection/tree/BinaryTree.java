8b60d92cc06abcbf9424ed8576748f895b06821c
delskev <kevindelsaux@gmail.com>
2014-11-14 22:05:10 +0100

    refactoring the project and migrate it to netbeans

---
+++ dungeonUtils/src/be/tdev/dungeonUtils/collection/tree/BinaryTree.java
@@ -0,0 +1,28 @@
+package be.tdev.collection.tree;
+
+/**
+ * Created by delskev on 14/11/2014.
+ */
+public class BinaryTree {
+    private Node root;
+    private Node focusNode;
+
+
+
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node root) {
+        this.root = root;
+    }
+
+    public Node getFocusNode() {
+        return focusNode;
+    }
+
+    public void setFocusNode(Node focusNode) {
+        this.focusNode = focusNode;
+    }
+}
